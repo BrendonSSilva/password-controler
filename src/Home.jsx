939ec82b99ca0025b364f51d126e810e14a2a7db
Brendon Silva <brendon.dasilva03@gmail.com>
2023-05-15 11:58:11 -0300

    cleaning code

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,25 +13,14 @@ class Home extends Nullstack {
 
 	render() {
 		return (
-			<div class="wrap-login100">
-				<form class="login100-form validate-form">
-					<div class="wrap-input100 validate-input">
-						<input class="input100" type="text" name="email" placeholder='E-Mail' required />
-					</div>
-					<div class="wrap-input100 validate-input">
-						<img src={this.visible ? './view.svg' : './hide.svg'} onclick={this.changeVisible} class="btn-show-pass" />
-						<input class="input100" type={this.visible ? 'password' : 'text'} placeholder='Password' name="pass" required />
-					</div>
-					<div class="container-login100-form-btn">
-						<div class="wrap-login100-form-btn">
-							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn">
-								Login
-							</button>
-						</div>
-					</div>
-				</form>
-			</div>
+			<form class="wrap-input100">
+				<input class="input100" type="text" name="email" placeholder='E-Mail' required />
+				<img src={this.visible ? './view.svg' : './hide.svg'} onclick={this.changeVisible} class="btn-show-pass" />
+				<input class="input100" type={this.visible ? 'password' : 'text'} placeholder='Password' name="password" required />
+				<button class="login100-form-btn">
+					Login
+				</button>
+			</form>
 		)
 	}
 
